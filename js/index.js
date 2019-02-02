c72f07f2591d049fa8e39dcfbd3c13c643f5a032
ermashkevich <olyabelka1993@gmail.com>
2019-02-02 23:54:20 +0300

    Burger

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-(function($){
-    //code here
-})(jQuery);
+// (function($){
+//     $(".burger").on("click", function() {
+//     	$(".menuPanel").toggleClass("open");
+//     });
+// })(jQuery);
+
+$(".burger").click(function() {
+	$(".menuPanel").toggleClass("open");
+	$(this).toggleClass("active");
+});
